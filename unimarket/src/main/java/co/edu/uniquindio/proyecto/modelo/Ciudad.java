7ad42b0726a28f695ebceed343e7245344cbfbda
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-03-04 14:24:12 -0500

    Create Ciudad.java
    Se suben una parte de las entidades de JPA

---
+++ unimarket/src/main/java/co/edu/uniquindio/proyecto/modelo/Ciudad.java
@@ -0,0 +1,44 @@
+package co.edu.uniquindio.proyecto.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Ciudad implements Serializable{
+    @Id
+    private String codigo;
+
+    private String nombre;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Ciudad ciudad = (Ciudad) o;
+
+        return codigo.equals(ciudad.codigo);
+    }
+
+    @Override
+    public int hashCode() {
+        return codigo.hashCode();
+    }
+}
