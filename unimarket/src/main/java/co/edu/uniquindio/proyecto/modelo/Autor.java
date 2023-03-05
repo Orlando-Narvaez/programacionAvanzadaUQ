8c6e820177e15c804766b7792e7d0d8e2f5c0a49
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-03-05 15:26:38 -0500

    Update Autor.java

--- unimarket/src/main/java/co/edu/uniquindio/proyecto/modelo/Autor.java
+++ unimarket/src/main/java/co/edu/uniquindio/proyecto/modelo/Autor.java
@@ -2,43 +2,19 @@ package co.edu.uniquindio.proyecto.modelo;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.*;
+
 import java.io.Serializable;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Autor implements  Serializable{
     @Id
     private String codigo;
 
     private String nombre;
-
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Autor autor = (Autor) o;
-
-        return codigo.equals(autor.codigo);
-    }
-
-    @Override
-    public int hashCode() {
-        return codigo.hashCode();
-    }
 }
