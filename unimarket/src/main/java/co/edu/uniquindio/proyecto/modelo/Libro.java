c9e299afea7be6ad39af255fe6031a19b910855c
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-03-05 15:26:47 -0500

    Update Libro.java

--- unimarket/src/main/java/co/edu/uniquindio/proyecto/modelo/Libro.java
+++ unimarket/src/main/java/co/edu/uniquindio/proyecto/modelo/Libro.java
@@ -1,10 +1,19 @@
 package co.edu.uniquindio.proyecto.modelo;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
+import lombok.*;
+
 import java.io.Serializable;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Libro implements Serializable {
     @Id
     private String isbn;
@@ -13,62 +22,8 @@ public class Libro implements Serializable {
 
     private String codigoGenero;
 
+    @Enumerated(value = EnumType.STRING)
     private int unidades;
 
     private String anio;
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getCodigoGenero() {
-        return codigoGenero;
-    }
-
-    public void setCodigoGenero(String codigoGenero) {
-        this.codigoGenero = codigoGenero;
-    }
-
-    public int getUnidades() {
-        return unidades;
-    }
-
-    public void setUnidades(int unidades) {
-        this.unidades = unidades;
-    }
-
-    public String getAnio() {
-        return anio;
-    }
-
-    public void setAnio(String anio) {
-        this.anio = anio;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Libro libro = (Libro) o;
-
-        return isbn.equals(libro.isbn);
-    }
-
-    @Override
-    public int hashCode() {
-        return isbn.hashCode();
-    }
 }
